114be59dcebc4e049612d63ce368a6263f5fa906
czy <647486848@qq.com>
2026-06-15 20:14:50 +0800

    ui替换

--- WaterSort/Assets/Scripts/GM/GM.cs
+++ WaterSort/Assets/Scripts/GM/GM.cs
@@ -137,7 +137,16 @@ public class GM : MonoBehaviour
         //};
         //UIManager.Instance.OpenAsync<UIWithdrawProgress>(EUIType.EUIWithdrawalProgress, UIOpenType.None, null, wItem);
 
-        UIManager.Instance.OpenAsync<UILuckyReward>(EUIType.EUILuckyReward);
+        WithdrawalRecordItem testItem = new WithdrawalRecordItem();
+        testItem.OrderId = 999;
+        testItem.LevelId = 999;
+        testItem.CreatedDate = "9999-99-99";
+        testItem.WRState = EWithRecordState.UnderReview;
+        testItem.WRMoney = 999;
+        testItem.TargetType = WithdrawTarget.AmountOfMoney;
+        FacadeWithdraw.SetCurWithdrawTarget(WithdrawTarget.AmountOfMoney);
+
+        UIManager.Instance.OpenAsync<UIWithdrawProgress>(EUIType.EUIWithdrawProgress, UIOpenType.None, null, testItem);
     }
 
 }
