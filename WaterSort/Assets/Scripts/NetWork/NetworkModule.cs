07bee950f4f0893c46433916e1e57d2ee24893b3
DESKTOP-K37NBRN\Administrator <647486848@qq.com>
2026-06-30 11:09:55 +0800

    通过网络控制转盘检测次数

--- WaterSort/Assets/Scripts/NetWork/NetworkModule.cs
+++ WaterSort/Assets/Scripts/NetWork/NetworkModule.cs
@@ -270,6 +270,10 @@ namespace XrCode
                 {
                     GameDefines.ClockTimeArr = Array.ConvertAll(cta.ToString().Split(','), float.Parse);
                 }
+                if(adDic.TryGetValue("SpinCount", out object sc2))
+                {
+                    GameDefines.SpinCount = int.Parse(sc2.ToString());
+                }
             }
         }
 
